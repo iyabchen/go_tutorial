c5ed2b3eca28fdf2f0ede90916107f98167f5b1d
Iris <Iris>
2017-01-26 01:49:46 +0000

    strconv Atoi, Itoa

---
+++ strconv.go
@@ -0,0 +1,21 @@
+// print.go
+
+package main
+
+import (
+	alias "fmt"
+	"strconv"
+)
+
+const PI = 3.14
+
+func main() {
+
+	var c int = 65
+	d := string(c)
+	alias.Println(d)
+	e := strconv.Itoa(c)
+	alias.Println(e)
+	f, _ := strconv.Atoi(e)
+	alias.Println(f)
+}
